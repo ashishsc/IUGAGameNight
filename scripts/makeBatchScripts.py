10dfe3b41f5520df2b456f5c6af92a292a37a96e
josh <jvillars19@gmail.com>
2013-04-12 13:45:13 -0700

    put the real, badass, l33thaxorz makeBatchScripts in

--- scripts/makeBatchScripts.py
+++ scripts/makeBatchScripts.py
@@ -1,2 +1,12 @@
 def win():
-	print 'hello'
+	 #"C:\Program Files\7-Zip\7z.exe" a -t7z files.7z "C:\Program Files (x86)\Steam\SteamApps\appmanifest_730.acf" -oC:\
+	for i in range(1,43):
+
+		builder = 'start psexec \\\\IS-M430M27-'
+		if i < 10 :
+			builder = builder + '0'
+		builder = builder + str(i) + " \"C:\\Program Files\\7-Zip\\7z.exe\" a -t7z files.7z \"C:\\Program Files (x86)\\Steam\\SteamApps\\appmanifest_730.acf\""
+		print builder
+		
+
+win();
