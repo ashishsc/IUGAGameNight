fb05e5bd00496b1ff0073928986a55b8da5abc2c
ashishsc <ashish.s.chandwani@gmail.com>
2013-04-17 20:53:46 -0700

    Added insteam.bat to do singular install of steam
    Eventually will write script that reads in file and calls insteam
    multiple times

---
+++ scripts/insteam.bat
@@ -0,0 +1,39 @@
+:: Ashish Chandwani - IUGA - Srping 2013
+:: This file is used to install steam in a given computer lab
+:: insteam room_num computer_num steam_installer_location
+:: Sample run:
+:: start psexec \\IS-M430M27-02 -c -v -d -s -i msiexec /i  \\IS-M430M27-21\gamenight\PsTools\SteamInstall.msi /q
+:: REQUIRES psexec
+:: Please make sure computers are restarted beforehand
+:: instloc cannot be a network location
+
+@echo off
+
+:: get number of args
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+IF NOT "%argC%"=="3" GOTO ERROR
+
+:: Assemble
+set PREFIX=\\IS-M
+set MIDFIX=M27-
+set room=%1
+set instloc=%3
+set computer=%PREFIX%%room%%MIDFIX%%2
+
+
+echo installing to %computer%
+echo from %instloc%
+
+:: quietly install
+start psexec %computer% -c -v -d -s -i msiexec /i  %instloc%\SteamInstall.msi /q
+GOTO END
+
+:ERROR
+echo %0:ERROR bad params!
+
+:USAGE
+echo usage: insteam room_num computer_num steam_installer_location
+echo to install steam
+
+:END
